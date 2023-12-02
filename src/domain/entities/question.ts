46daabd92889537d9fec9de8517d4db56962ded9
vinicius <viniciusvwh1441@gmail.com>
2023-12-02 15:20:25 -0300

    initiation & base DDD

---
+++ src/domain/entities/question.ts
@@ -0,0 +1,25 @@
+import { randomUUID } from "crypto";
+import { Slug } from "./value-objects/slug";
+
+interface QuestionProps {
+  title: string,
+  content: string,
+  authorId: string,
+  slug: Slug
+}
+
+export class Question {
+  public id: string
+  public title: string
+  public content: string
+  public authorId: string
+  public slug: Slug
+
+  constructor(props: QuestionProps, id?: string) {
+    this.title = props.title;
+    this.content = props.content;
+    this.authorId = props.authorId;
+    this.slug = props.slug;
+    this.id = id ?? randomUUID()
+  }
+}
