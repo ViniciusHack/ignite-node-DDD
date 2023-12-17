ed0aa290ee5135708764c0ce26698535cb3daf51
vinicius <viniciusvwh1441@gmail.com>
2023-12-17 11:46:54 -0300

    feat: fetch recent questions usecase

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
@@ -28,5 +29,13 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     const question = this.items.find((item) => item.slug.value === slug)
 
     return question ?? null
+  }
+
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice(page - 1 * 20, page * 20)
+
+    return questions
   }
 }
