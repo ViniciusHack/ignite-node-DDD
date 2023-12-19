acae75dcb9aae664485d7323d17491b27459cd9e
vinicius <viniciusvwh1441@gmail.com>
2023-12-18 23:54:14 -0300

    feat: comment on answer use case and delete question comment use case

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -4,6 +4,18 @@ import { QuestionComment } from '@/domain/forum/enterprise/entities/question-com
 export class InMemoryQuestionCommentsRepository
   implements QuestionCommentsRepository
 {
+  async findById(id: string) {
+    return this.items.find((item) => item.id.toString() === id) ?? null
+  }
+
+  async delete(questionComment: QuestionComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id,
+    )
+
+    this.items.splice(itemIndex, 1)
+  }
+
   public items: QuestionComment[] = []
 
   async create(questionComments: QuestionComment) {
